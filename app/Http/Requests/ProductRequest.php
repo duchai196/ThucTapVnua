9f2595e23602e9958f50060e7c2ebe925d3f31d5
Luan <smallmonster900@gmail.com>
2018-01-08 13:16:29 +0700

    Luận tạo product request

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=>'required|max:255|min:3|unique:products'
+        ];
+    }
+    public  function  messages()
+    {
+       return [
+         'name.required'=>'Bạn chưa nhập tên sản phẩm',
+         'name.max'=>'Tên không vượt quá 255 ký tự',
+         'name.min'=>'Tên không ngắn hơn 3 ký tự',
+         'name.unique'=>'Sản phẩm đã tồn tại'
+       ];
+    }
+}
