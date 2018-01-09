1e7e033bbca69974fbe0f13f4c158174bd5b318d
Hai Duc Nguyen <duchai196@gmail.com>
2018-01-09 22:25:33 +0700

    Upload lại code -_-

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -24,7 +24,8 @@ class ProductRequest extends FormRequest
     public function rules()
     {
         return [
-            'name'=>'required|max:255|min:3|unique:products'
+            'name' => 'required|max:255|min:3|unique:products',
+            'cate_id' => 'required'
         ];
     }
     public  function  messages()
@@ -33,7 +34,8 @@ class ProductRequest extends FormRequest
          'name.required'=>'Bạn chưa nhập tên sản phẩm',
          'name.max'=>'Tên không vượt quá 255 ký tự',
          'name.min'=>'Tên không ngắn hơn 3 ký tự',
-         'name.unique'=>'Tên sản phẩm đã tồn tại'
+           'name.unique' => 'Tên sản phẩm đã tồn tại',
+           'cate_id.required' => 'Bạn chưa chọn danh mục sản phẩm'
        ];
     }
 }
