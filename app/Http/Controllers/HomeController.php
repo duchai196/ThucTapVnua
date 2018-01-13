0fd309ed0b004b258a99cb0fbf7b813b4e31785d
Hai Duc Nguyen <duchai196@gmail.com>
2018-01-14 04:18:22 +0700

    do du lieu ra view

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -29,8 +29,15 @@ class HomeController extends Controller
     {
         $banners = Banner::where('status', 1)->limit(3)->orderBy('id', 'DESC')->get();
         $newProduct = DB::table('products')->orderBy('created_at', 'desc')->take(10)->get();
+        $lastestProduct = DB::table('products')->orderBy('created_at', 'desc')->first();
+        $listCate = DB::table('categories')->get();
         $saleProduct = DB::table('products')->where('sale_price', '!=', 0)->take(10)->get();
-        return view('frontend.pages.index', compact('banners', 'newProduct', 'saleProduct'));
+
+
+        $hotDeal = DB::table('products')->select('id', 'price', 'sale_price', 'image', 'name')->where([['sale_price', '!=', null], ['price', '!=', null], ['status', 1]])->take(10)->get();
+
+
+        return view('frontend.pages.index', compact('banners', 'newProduct', 'saleProduct', 'hotDeal'));
     }
 
     public function post()
